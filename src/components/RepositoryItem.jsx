80f9dd2fef2342d6eeee31dd2348673d9d88b76b
Hudson Holanda <hudsonholanda55@gmail.com>
2021-09-13 18:58:48 -0300

    refact: change prop from url repository and remove default string

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository.name ?? "default"}</strong>
-      <p>{props.repository.description ?? "default"}</p>
-      <a href={props.repository.link ?? "default"}>Acessar repositório</a>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>Acessar repositório</a>
     </li>
   );
 }
