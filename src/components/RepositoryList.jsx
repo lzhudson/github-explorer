f37e8f2e2d66cfe22068ac8bfdeb6ff1e0de676c
Hudson Holanda <hudsonholanda55@gmail.com>
2021-09-13 18:53:28 -0300

    feat: fetch repositories from api github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,15 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
-const repositoryName = "unform2";
+import { useState, useEffect } from "react";
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
